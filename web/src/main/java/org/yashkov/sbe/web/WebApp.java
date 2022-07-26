75906539670b9d7264bb9a6ab180b263c0d76b96
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-07-26 18:22:04 -0400

    Add core configuration, correct the main class name

--- web/src/main/java/org/yashkov/sbe/web/WebApp.java
+++ web/src/main/java/org/yashkov/sbe/web/WebApp.java
@@ -2,11 +2,14 @@ package org.yashkov.sbe.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.yashkov.sbe.core.CoreConfig;
 
 @SpringBootApplication
 @EnableHypermediaSupport(type = HypermediaType.HAL)
+@Import(CoreConfig.class)
 public class WebApp {
     public static void main(String[] args)
     {
